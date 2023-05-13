45be3b3b83fc7dc31fbe2ca202bd6786041e4449
Rashid Farid <128982123+Rashid-Farid786@users.noreply.github.com>
2023-05-13 16:24:17 +0500

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rashidfarif/simple_php_crud',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '21d242093bab20166783ee81024e875d706ac28d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rashidfarif/simple_php_crud' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '21d242093bab20166783ee81024e875d706ac28d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
